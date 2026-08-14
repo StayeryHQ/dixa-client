3725c98cd3ae5182d80cdb16f5f213ba2776880e
Luca Herzke <james.lol2000@gmail.com>
2026-08-14 16:10:31 +0200

    chore: Export Error and Result type

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 mod generated;
 
 pub type DixaClient = generated::HttpClient;
+pub use generated::client::HttpError as DixaError;
+pub use generated::client::HttpResult as DixaResult;
 pub use generated::types::*;
